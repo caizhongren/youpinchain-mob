f3270d79e1da9d78649e2763e246dadfa924799e
yuyang <yuyang1@hooali.com>
2018-07-25 14:49:14 +0800

    feat(goods.vue):添加预售显示布局及分享蒙层

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -2,14 +2,14 @@
   <header id='head_top'>
     <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
-          <polyline points="12,18 4,9 12,0" style="fill:none;stroke:#999;stroke-width:2"/>
+        <polyline points="12,18 4,9 12,0" style="fill:none;stroke:#999;stroke-width:2"/>
       </svg>
     </section>
-    <section v-if='isShare' class="head_share">
+    <section v-if='isShare' class="head_share" @click="$parent.showShare = true">
       <img src="../../images/share-icon.png" alt="" width="100%">
     </section>
-    <section class="title_head ellipsis" v-if="headTitle">
-        <span class="title_text">{{headTitle}}</span>
+    <section class="title_head ellipsis">
+      <span class="title_text">{{headTitle}}</span>
     </section>
   </header>
 </template>
--- src/page/goods/goods.vue
+++ src/page/goods/goods.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="goods">
+    <head-top class="header" go-back='true' is-share="true" :showShare="showShare" :headTitle="headTitle"></head-top>
     <div class="top_main">
-      <carousel :loop="true" :autoplay="true" :minSwipeDistance="10" :scrollPerPage="true" :speed="500" :perPage="1" :paginationPadding="10" :paginationSize="10" :paginationActiveColor="pagination.activeColor" :paginationColor="pagination.color">
+      <carousel :loop="true" :autoplay="true" :minSwipeDistance="6" :scrollPerPage="true" :speed="500" :perPage="1" :paginationPadding="5" :paginationSize="8" :paginationActiveColor="pagination.activeColor" :paginationColor="pagination.color">
         <slide>
           <img src="../../images/1.png" alt="" width="100%" class="show">
         </slide>
@@ -12,9 +13,16 @@
           <img src="../../images/1.png" alt="" width="100%" class="show">
         </slide>
       </carousel>
+      <div class="presell_box" v-if="goods.presell">
+        <div class="left_price left">
+          <p class="price"><span>¥</span>{{goods.price}} <s>¥{{goods.marketPrice}}</s></p>
+          <p class="tip">商品预售预计{{goods.presellTime | dateCharacter}}发货</p>
+        </div>
+        <div class="right_tip right">预售</div>
+      </div>
       <div class="title">
-        <p class="price"><span>¥</span>{{goods.price}} <s>¥{{goods.marketPrice}}</s></p>
-        <p class="name">{{goods.name}}</p>
+        <p class="price" v-if="!goods.presell"><span>¥</span>{{goods.price}} <s>¥{{goods.marketPrice}}</s></p>
+        <p class="name"><span class="presell_text" v-if="goods.presell">【预售】</span>{{goods.name}}</p>
         <p class="desr">{{goods.description}}</p>
       </div>
     </div>
@@ -28,51 +36,85 @@
       </router-link>
       <div class="cart_btn right" @click="addCartList(goods)">加入购物车</div>
     </div>
+    <share-mask v-if="showShare" :showShare="showShare"></share-mask>
   </div>
 </template>
 
 <script>
   import { Carousel, Slide } from 'vue-carousel';
+  import shareMask from 'src/components/common/shareMask'
+  import headTop from 'src/components/header/head'
+  import { ModalHelper } from '../../service/Utils'
   export default {
     data(){
       return{
+        showShare: false,
+        headTitle: '',
         goodsid:'',
-        goods: {
-          name:'猪耳朵500g*1份',
-          description: '和黄瓜丝凉拌好吃极了～',
-          price: 23.99,
-          marketPrice: 33.99,
-          Desc: '<div>商品介绍内容 <br>商品介绍内容 </br>商品介绍内容</div>'
-        },
+        goods: {},
         cart_num: 8,
         number: [1, 2, 3, 4, 5],
         pagination: {
           activeColor: '#e4372e',
-          color: '#ddd'
+          color: '#fff'
         }
+      }
+    },
+    watch: {
+      'showShare': function (newVal, oldVal) {
+        newVal ? ModalHelper.afterOpen() : ModalHelper.beforeClose()
       }
     },
     mounted(){
       this.goodsid = this.$route.params.goodsid;
       this.initData();
+      var that = this
+      window.onscroll = function () {
+        var t = document.documentElement.scrollTop || document.body.scrollTop
+        if (t >= 30) {
+          that.headTitle = that.goods.name;
+        } else {
+          that.headTitle = ''
+        }
+      }
     },
     components:{
       Carousel,
-      Slide
+      Slide,
+      shareMask,
+      headTop
     },
     computed:{
     },
     methods:{
       initData () {
+        if (this.goodsid === '0') {
+          this.goods = {
+            name:'猪耳朵500g*1份',
+            description: '和黄瓜丝凉拌好吃极了～',
+            price: 23.99,
+            marketPrice: 33.99,
+            presellTime: 11111111,
+            presell: true, // 是否预售
+            Desc: '<div>商品介绍内容 <br>商品介绍内容 </br>商品介绍内容</div>'
+          }
+        } else {
+          this.goods = {
+            name:'猪耳朵500g*2份',
+            description: '和黄瓜丝凉拌好吃极了～',
+            price: 23.99,
+            marketPrice: 33.99,
+            presellTime: 11111111,
+            presell: false, // 是否预售
+            Desc: '<div>商品介绍内容 <br>商品介绍内容 </br>商品介绍内容</div>'
+          }
+        }
       },
       addCartList (goods) {
         this.cart_num += 1;
       },
-      onSlideChangeStart (currentPage) {
-        console.log('onSlideChangeStart', currentPage);
-      },
-      onSlideChangeEnd (currentPage) {
-        console.log('onSlideChangeEnd', currentPage);
+      onScroll () {
+        console.log(onScroll)
       }
     }
   }
@@ -81,11 +123,41 @@
 <style lang="scss" scoped>
   @import 'src/style/mixin';
   .goods {
-    padding-bottom: .5rem;
+    padding: .45rem 0 .5rem;
+    .presell_box {
+      overflow: hidden;
+      padding: .08rem .25rem .12rem .12rem;
+      background-image: linear-gradient(133deg, #fc5b46, #fa424f);
+      @include wh(100%, .64rem);
+      .left_price {
+        .price {
+          @include sc(.238rem, $fc);
+          font-weight: bold;
+          span { 
+            @include sc(.15rem, $fc);
+            font-weight: bold; 
+          }
+          s { 
+            @include sc(.12rem, $fc);
+            font-weight: normal; 
+          }
+        }
+        .tip {
+          @include sc(.12rem, $fc);
+          margin-top: -.05rem;
+        }
+      }
+      .right_tip {
+        @include sc(.18rem, $fc);
+        padding: .04rem 0 .04rem .3rem;
+        margin-top: .08rem;
+        border-left: 1px solid $fc;
+      }
+    }
     .title {
       background-color: $fc;
       padding: 0.02rem .15rem .1rem;
-      border-radius: 10px 10px 0 0;
+      border-radius: .12rem .12rem 0 0;
       .price {
         @include sc(.238rem, $red);
         font-weight: bold;
@@ -101,6 +173,11 @@
       .name {
         @include sc(.18rem, $g3);
         padding: .01rem 0 0.02rem;
+        .presell_text {
+          @include sc(.18rem, $g3);
+          margin: .08rem 0 0 -.1rem;
+          display: inline-block;
+        }
       }
       .desr {
         @include sc(.12rem, $g6);
