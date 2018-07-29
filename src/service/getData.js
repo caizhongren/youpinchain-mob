454da766de8c34f292c38646bf275f0944ce813b
zhangfeng <zhangfeng@hoolai.com>
2018-07-30 02:06:46 +0800

    feat(getData)：更新接口

--- src/service/getData.js
+++ src/service/getData.js
@@ -8,17 +8,20 @@ export const homeIndex = () => fetch('/home/index', {
 });
 
 /**
- * 添加购物车
+ * 
  * @param {商品id} productId 
+ * @param {数量} number 
  */
-
-export const addToCart = productId => fetch('carts/products' + productId, {
-
+export const addToCart = (productId, number) => fetch('carts', {
+	productId: productId,
+	number: number
 }, 'POST')
 
 
 /**
  * 获取首页热卖商品
+ * @param {页数} page
+ * @param {每页数量} size
  */
 
 export const hotgoods = (page, size) => fetch('/wx/home/index', {
@@ -31,14 +34,16 @@ export const hotgoods = (page, size) => fetch('/wx/home/index', {
  */
 
 export const hotcity = (page, size) => fetch('/wx/wx/photo/list', {
-
+	page: page,
+	size: size
 });
 
 /**
  * 查询商品列表
+ * @param {品牌id} brandId
  */
 
-export const productList = (brandId, page, size) => fetch('/youpin/wx/product/list', {
+export const productList = (brandId, page, size) => fetch('/products', {
 	brandId: brandId,
 	page: page,
 	size: size
@@ -47,48 +52,63 @@ export const productList = (brandId, page, size) => fetch('/youpin/wx/product/li
 
 /**
  * 根据商品id查询商品详情
+ * @param {商品id} proId
  */
-export const findProduct = (proId) => fetch('/youpin/wx/product/findProduct', {
-	proId: proId
+export const getProductDetail = (proId) => fetch('/products/' + proId, {
 });
 
 
 /**
  * 查询用户购物车
  */
-export const findCart = (page, size) => fetch('/youpin/wx/cart/findCart', {
+export const findCart = (page, size) => fetch('/carts', {
 	page: page,
 	size: size
 });
 
+/**
+ * 更新购物车
+ * @param {购物车Id} cartId
+ * @param {数量} number
+ */
+export const updateCart = (cartId, number) => fetch('/carts/' + cartId, {
+	number: number
+}, 'PUT');
+
+/**
+ * 删除购物车
+ */
+export const deleteCart = (cartId) => fetch('/carts/' + cartId, {}, 'DELETE');
+
 
 /**
  * 添加购物车
+ * @param {商品id} productId
+ * @param {数量} number
  */
-export const addCart = (youpinCar, productId, number) => fetch('/youpin/wx/cart/add', {
-	youpinCar: youpinCar,
+export const addCart = (youpinCar, productId, number) => fetch('/carts', {
 	productId: productId,
-	number:  number
+	number: number
 }, 'POST');
 
 
 /**
  * 地址列表查询
  */
-export const getAddressList = () => fetch('/youpin/wx/address');
+export const getAddressList = () => fetch('/addresses');
 
 
 /**
  * 地址详情
  */
-export const getAddressDetail = (addressid) => fetch('/youpin/wx/address/' + addressid);
+export const getAddressDetail = (addressId) => fetch('/addresses/' + addressId);
 
 
 /**
- * 添加或更新收货地址
+ * 添加收货地址
  */
 
-export const postAddAddress = (name, provinceId, cityId, districtId, mobile, address) => fetch('/youpin/wx/address', {
+export const addAddress = (name, provinceId, cityId, districtId, mobile, address) => fetch('/addresses', {
 	name,
 	provinceId,
 	cityId,
@@ -97,26 +117,39 @@ export const postAddAddress = (name, provinceId, cityId, districtId, mobile, add
 	address
 }, 'POST');
 
+/**
+ * 更新收货地址
+ */
+
+export const updateAddress = (addressId, name, provinceId, cityId, districtId, mobile, address) => fetch('/addresses/' + addressId, {
+	name,
+	provinceId,
+	cityId,
+	districtId,
+	mobile,
+	address
+}, 'PUT');
+
 
 /**
 * 删除收货地址
 */
 
-export const deleteAddress = (addressid) => fetch( '/youpin/wx/address/' + addressid, {}, 'DELETE')
+export const deleteAddress = (addressId) => fetch('/addresses/' + addressId, {}, 'DELETE')
 
 
 /**
  * 微信授权登陆
  */
 
-export const login_oa = () => fetch('/youpin/wx/oa/login_oa');
+export const login_oa = () => fetch('/oa/login_oa');
 
 
 /**
  * 提交订单
  */
 
-export const submitOrder = ({cartIds, addressId}) => fetch('/youpin/wx/order/submit', {
+export const submitOrder = ({ cartIds, addressId }) => fetch('/orders', {
 	cartIds: cartIds,
 	addressId: addressId
 }, 'POST');
@@ -126,8 +159,7 @@ export const submitOrder = ({cartIds, addressId}) => fetch('/youpin/wx/order/sub
  * 确认收货
  */
 
-export const confirmOrder = ({orderId}) => fetch('/youpin/wx/order/confirm', {
-	orderId: orderId
+export const confirmOrder = ({ orderId }) => fetch('/orders/' + orderId + 'confirm', {
 }, 'POST');
 
 
@@ -135,8 +167,7 @@ export const confirmOrder = ({orderId}) => fetch('/youpin/wx/order/confirm', {
  * 预付款
  */
 
-export const prepayOrder = ({orderId}) => fetch('/youpin/wx/order/prepay', {
-	orderId: orderId
+export const prepayOrder = ({ orderId }) => fetch('/orders' + orderId + '/prepay', {
 }, 'POST');
 
 
@@ -145,7 +176,7 @@ export const prepayOrder = ({orderId}) => fetch('/youpin/wx/order/prepay', {
  *  showType 0:全部，1：待付款，2：待发货，3：待收货，4：待评价
  */
 
-export const getOrderList = (page, size, showType) => fetch('/youpin/wx/order', {
+export const getOrderList = (page, size, showType) => fetch('/orders', {
 	page: page,
 	size: size,
 	showType: showType
@@ -156,4 +187,16 @@ export const getOrderList = (page, size, showType) => fetch('/youpin/wx/order', 
  * 获取订单详情
  */
 
-export const getOrderDetail = (orderid) => fetch('/youpin/wx/order/' + orderid);
+export const getOrderDetail = (orderId) => fetch('/orders/' + orderId);
+
+/**
+ * 取消订单
+ */
+
+export const cancelOrder = (orderId) => fetch('/orders/' + orderId, {}, 'DELETE');
+
+/**
+ * 申请退款
+ */
+
+export const cancelOrder = (orderId) => fetch('/orders/' + orderId + 'refund', {}, 'POST');
