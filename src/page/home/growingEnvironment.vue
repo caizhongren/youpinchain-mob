d9405db0378c52a9b6b36a276f295fbdcac599f0
yuyang <yuyang1@hooali.com>
2018-08-02 14:54:45 +0800

    feat(growingEnvironment):生长环境添加返回菜单

--- src/page/home/growingEnvironment.vue
+++ src/page/home/growingEnvironment.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="growingEnvironment">
+  <div class="growingEnvironment header-top">
+      <head-top class="header" go-back='true' :headTitle="headTitle"></head-top>
       <img v-for="item in pics" :src="item" alt="" width="100%" class="show">
   </div>
 </template>
 <script>
-    import {brandsPics} from '../../service/getData'
+  import {brandsPics} from '../../service/getData'
+  import headTop from 'src/components/header/head'
   export default {
     data () {
       return {
         dataId:"",
-          pics:[]
+          pics:[],
+          headTitle: ''
       }
     },
     watch: {
@@ -26,10 +29,14 @@
     },
     created() {
         this.dataId = this.$route.query.dataId;
+        this.headTitle = this.dataId === '2' ? '生长环境' : this.dataId === '4' ? '饲养情况' : '健康指标'
     },
     methods: {
 
     },
+    components: {
+        headTop
+    }
   }
 </script>
 <style>
