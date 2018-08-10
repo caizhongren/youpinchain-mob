7fdca2a3a8d2ce574ad08838c87946cab3e633eb
yuyang <yuyang1@hooali.com>
2018-08-10 17:33:18 +0800

    feat(bounty):赏金首页布局加数据绑定

--- src/page/activity/bounty/children/home.vue
+++ src/page/activity/bounty/children/home.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="bounty_home">
-      <div class="top_header">
-          <router-link tag="div" class="fl" :to="'/bountyPlan/userCenter'"></router-link>
-          <div class="gold_box">
-              <router-link tag="p" :to="'/bountyPlan/userCenter/goldRecord/0'">金钻 {{data.balance.goldDrill}}</router-link>
-              <router-link tag="p" :to="'/bountyPlan/userCenter/goldRecord/1'">金条 {{data.balance.bullion}}</router-link>
-          </div>
-      </div>
-  </div>
+    <div class="bounty_home">
+        <div class="top_header">
+            <div class="clear">
+                <router-link tag="div" class="user_icon left" :to="'/bountyPlan/userCenter'"></router-link>
+                <div class="gold_box right">
+                    <router-link tag="p" :to="'/bountyPlan/userCenter/goldRecord/0'"><img src="../../../../images/bounty-plan/money_reward_icon1.png" width="30">金钻 {{calculate(data.balance.goldDrill)}}</router-link>
+                    <router-link tag="p" :to="'/bountyPlan/userCenter/goldRecord/1'"><img src="../../../../images/bounty-plan/money_reward_icon2.png" width="33%">金条 {{calculate(data.balance.bullion)}}</router-link>
+                </div>
+            </div>
+            <p class="text">精彩功能 敬请期待</p>
+        </div>
+        <div class="task_box">
+            <p class="task_title">领金任务</p>
+            <ul class="task_list">
+                <li>
+                    <p class="task_name">每日登录</p>
+                    <img src="../../../../images/bounty-plan/login_icon.png" class="icon">
+                    <p class="desr">今日登录+15金条</p>
+                    <p class="status_finish"><img src="../../../../images/bounty-plan/finished.png" width="10%"> 已完成</p>
+                </li>
+                <router-link tag="li" :to="'/bountyPlan/invite'">
+                    <p class="task_name">邀请好友</p>
+                    <img src="../../../../images/bounty-plan/invite_icon.png" class="icon">
+                    <p class="desr">邀请1位好友+10金条</p>
+                    <p class="reward">最多+150<img src="../../../../images/bounty-plan/money_reward_icon2.png" width="10%"></p>
+                </router-link>
+                <router-link tag="li" :to="'/bountyPlan/waiter'">
+                    <p class="task_name">添加客服微信</p>
+                    <img src="../../../../images/bounty-plan/wx_icon.png" class="icon">
+                    <p class="desr"> 添加成功获得</p>
+                    <p class="status_finish" v-if="data.waiter"><img src="../../../../images/bounty-plan/finished.png" width="10%"> 已完成</p>
+                    <p class="reward" v-if="!data.waiter"><img src="../../../../images/bounty-plan/money_reward_icon2.png" width="10%">+500</p>
+                </router-link>
+            </ul>
+        </div>
+        <div class="task_box marg-t-15">
+            <p class="task_title">淘金任务</p>
+            <ul class="task_list">
+                <router-link tag="li" :to="'/bountyPlan/task'" v-for="(task,index) in data.task" :key="index" class="position-re">
+                    <img src="../../../../images/bounty-plan/new_activity.png" class="new_icon">
+                    <img :src="task.themePic" class="task_icon">
+                    <p class="task_name">{{task.name}}</p>
+                    <p class="status_finish" v-if="data.taskUser !== null"><img src="../../../../images/bounty-plan/finished.png" width="10%"> 已完成</p>
+                    <p class="reward task_reward" v-if="data.taskUser === null"><img src="../../../../images/bounty-plan/money_reward_icon1.png" width="10%"> +{{task.rewardPrice}}</p>
+                </router-link>
+                <li>
+                    <img src="../../../../images/bounty-plan/coming_soon.png" class="coming_soon">
+                    <p class="task_name">敬请期待</p>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 <script>
     import { bountyHome } from '../../../../service/getData'
@@ -30,18 +73,149 @@
             //获取首页信息
             bountyHome().then(res => {
                 this.data = res.data
+                console.log(this.data.taskUser === null)
             })
         },
         created() {
             
         },
         methods: {
-
+            calculate: function (val) {
+                val > 10000 ? val = Math.round(val / 10000 * 100) / 100 + '万' : val = val;
+                return val
+            }
+        },
+        computed: {
         },
     }
 </script>
-<style scoped>
+<style lang="scss" scoped>
+  @import '../../../../style/mixin';
    .bounty_home {
-
+        .marg-t-15 {
+           margin-top: .15rem;
+        }
+       background: $bc;
+        .top_header {
+           padding: .22rem .2rem;
+            @include wh(100%, 1.73rem);
+           @include bis('../../../../images/bounty-plan/starry_sky_bg3.png');
+           .user_icon {
+               @include wh(.27rem, .27rem);
+               @include bis('../../../../images/bounty-plan/user_icon.png');
+           }
+           .gold_box {
+               display: flex;
+               align-items: center;
+               text-align: center;
+               border-radius: .163rem;
+               padding: 0 .05rem;
+               margin-right: -.1rem;
+               @include wh(2.65rem, .325rem);
+               background: rgba(255, 255, 255, 0.4);
+               color: $fc;
+               p {
+                   display: inline-block;
+                   letter-spacing: 0.1px;
+                   font-size: .11rem;
+                   text-align: left;
+                   width: 50%;
+                   margin-top: .03rem;
+                   img {
+                       vertical-align: middle;;
+                   }
+               }
+           }
+            .text {
+                text-align: center;
+                @include sc(.3rem, $fc);
+                margin-top: .28rem;
+            }
+        }
+        .task_box {
+            background: $fc;
+            border-radius: .1rem;
+            padding: .1rem .15rem 0;
+            .task_title {
+                @include sc(.15rem, $g3);
+                border-bottom: 1px solid $gd;
+                padding-bottom: .1rem;
+            }
+            .task_list {
+                overflow: hidden;
+                width: 100%;
+                li {
+                    float: left;
+                    width: 36%;
+                    text-align: center;
+                    border-right: 1px solid $gd;
+                    padding: .1rem 0 .1rem;
+                    .icon {
+                        @include wh(.345rem, .345rem);
+                        vertical-align: middle;
+                        margin: .05rem auto;
+                    }
+                    .task_icon {
+                        @include wh(.62rem, .62rem);
+                        vertical-align: middle;
+                        border-radius: 50%;
+                    }
+                    .task_name {
+                        @include sc(.14rem, $g3);
+                        margin-top: .08rem;
+                    }
+                    .desr {
+                        letter-spacing: 0.5px;
+                        @include sc(.1rem, $g6);
+                        transform: scale(.85);
+                    }
+                    .status_finish {
+                        @include sc(.13rem, $g6);
+                        @include wh(1.065rem, .23rem);
+                        margin: 0.06rem auto 0;
+                        img {
+                            width: 11%;
+                            vertical-align: middle;
+                        }
+                    }
+                    .reward {
+                        @include sc(.14rem, $g6);
+                        @include wh(1.02rem, .25rem);
+                        line-height: .25rem;
+                        border-radius: .025rem;
+                        margin: 0.04rem auto 0;
+                        background-color: #ffeeec;
+                        text-align: center;
+                        img {
+                            width: 28%;
+                            vertical-align: middle;
+                        }
+                    }
+                    .task_reward {
+                        text-align: left;
+                        padding-left: .1rem;
+                    }
+                    .coming_soon {
+                        width: 45%;
+                        margin: .45rem auto .26rem;
+                    }
+                    .new_icon {
+                        position: absolute;
+                        width: 21%;
+                        right: 14%;
+                        top: 0;
+                    }
+                }
+                li:first-child {
+                    padding-right: .1rem;
+                    width: 32%;
+                }
+                li:last-child {
+                    border: none;
+                    padding-left: .1rem;
+                    width: 32%;
+                }
+            }
+        }
    }
 </style>
