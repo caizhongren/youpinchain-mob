2b0416f6f80d054c06d83981251c0d5e69f918e2
zhangfeng <zf798904060@163.com>
2018-08-24 11:57:03 +0800

    feat(百度统计)：添加百度统计代码，youpinlian和zhenpin不一样

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,7 +55,8 @@ var webpackConfig = merge(baseWebpackConfig, {
             //         // https://github.com/kangax/html-minifier#options-quick-reference
             // },
             // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-            chunksSortMode: 'dependency'
+            chunksSortMode: 'dependency',
+            baidutongji: 'de5c813bb399ca6786d0d185a8c20259'
         }),
         // split vendor js into its own file
         new webpack.optimize.CommonsChunkPlugin({
